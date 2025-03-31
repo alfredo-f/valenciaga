18eb210d6fb176167316e026ed6ff70c71e74a21
Alfredo Fomitchenko <a.fomitchenko@reply.it>
2025-04-01 00:01:28 +0200

    i

--- prompts/print_contents.py
+++ prompts/print_contents.py
@@ -168,7 +168,7 @@ def main(
         write_to_output(
             dedent(r"""
 <section_of_your_task>
-I want the image to be a centered thing, where the text is placed on top of it, 50px from top
+Make the entire thing centered horizontally
 </section_of_your_task>
                 """)
         )
